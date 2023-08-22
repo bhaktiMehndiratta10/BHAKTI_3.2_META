e7648f2fcdbfc3019e29b88240342fe375902e07
bhaktiMehndiratta10 <138040735+bhaktiMehndiratta10@users.noreply.github.com>
2023-08-23 04:25:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,34 @@
-const contractAddress = '0xd9145CCE52D386f254917e481eB44e9943F39138'; 
-const abi = [[
-	{
-		"inputs": [],
-		"name": "increment",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_value",
-				"type": "uint256"
-			}
-		],
-		"name": "set",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "get",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "storedValue",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
-]]; 
+const contractAddress = 'CONTRACT_ADDRESS'; // Replace with your contract address
+const abi = [...]; // Replace with your contract ABI
 
 const web3 = new Web3(Web3.givenProvider);
 web3.eth.defaultAccount = web3.eth.accounts[0]; // Set default account
-
 const simpleContract = new web3.eth.Contract(abi, contractAddress);
 
 function setValue() {
+    console.log('setValue() called'); // Debugging log
     const newValue = parseInt(document.getElementById('newValue').value);
     simpleContract.methods.set(newValue).send({ from: web3.eth.defaultAccount })
-    .then(() => console.log('Value set successfully'));
+    .then(() => {
+        console.log('Value set successfully'); // Debugging log
+        document.getElementById('resultDisplay').textContent = 'Value set successfully';
+    });
 }
 
 function getValue() {
+    console.log('getValue() called'); // Debugging log
     simpleContract.methods.get().call()
     .then(value => {
+        console.log('Value retrieved:', value); // Debugging log
         document.getElementById('valueDisplay').textContent = `Current Value: ${value}`;
     });
 }
 
 function incrementValue() {
+    console.log('incrementValue() called'); // Debugging log
     simpleContract.methods.increment().send({ from: web3.eth.defaultAccount })
-    .then(() => console.log('Value incremented successfully'));
+    .then(() => {
+        console.log('Value incremented successfully'); // Debugging log
+        document.getElementById('resultDisplay').textContent = 'Value incremented successfully';
+    });
 }
